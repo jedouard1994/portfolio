a7acb01a752fdfe297e211e534d3ddb6a88cd946
James Edouard <jedouard1994@gmail.com>
2017-01-03 16:43:10 -0500

    completed euler problem001

---
+++ problems/euler/problem001/solve.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    if(argc != 1){
+        //ensure correct program usage
+        printf("usage: solve\n");
+        exit(1);
+    }
+    
+    int acc, i;
+
+    acc = 0;
+    for(i = 3; i < 1000; i++){
+        if((i%3 == 0) || (i%5 == 0)){
+            acc+=i;
+        }        
+    }
+    
+    printf("%d\n", acc);
+}
