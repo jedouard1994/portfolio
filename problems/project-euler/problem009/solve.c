4c45fdd5b0869ddc12910ca344df6fa29eebbf34
James Edouard <jedouard1994@gmail.com>
2017-01-05 15:03:53 -0500

    completed euler problem009

---
+++ problems/project-euler/problem009/solve.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int NUM_LIMIT = 1000;
+
+
+int main(int argc, char *argv[]){
+    if(argc != 1){
+        //ensure correct program usage
+        printf("usage: ./solve\n");
+        exit(1);
+    }
+    
+    int i, j, k, product;
+    
+    for(i = 0; i < NUM_LIMIT; i++){
+        for(j = i; j < NUM_LIMIT; j++){
+            for(k = j; k < NUM_LIMIT; k++){
+                if((i + j + k == 1000) && (i*i + j*j == k*k)){
+                    product = i*j*k;
+                    break;       
+                }
+            }
+        }
+    }
+
+    printf("%d\n", product);
+}
