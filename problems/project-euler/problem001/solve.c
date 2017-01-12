3c2f2ed59fa0d6eb4a3186a84a4ab155b5e03b0b
James Edouard <jedouard1994@gmail.com>
2017-01-12 16:35:35 -0500

    updated euler problem001

--- problems/project-euler/problem001/solve.c
+++ problems/project-euler/problem001/solve.c
@@ -1,21 +1,19 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include <iostream>
+#include <stdlib.h> 
 
 int main(int argc, char *argv[]){
     if(argc != 1){
         //ensure correct program usage
-        printf("usage: ./solve\n");
+        std::cout << "usage: ./solve" << std::endl;
         exit(1);
     }
     
-    int acc, i;
-
-    acc = 0;
-    for(i = 3; i < 1000; i++){
+    int acc = 0;
+    for(int i = 3; i < 1000; i++){
         if((i%3 == 0) || (i%5 == 0)){
             acc+=i;
         }        
     }
     
-    printf("%d\n", acc);
+    std::cout << acc << std::endl;
 }
